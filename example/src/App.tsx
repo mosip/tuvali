2266e58c4edf9d6e5dbc4b2afbdf338d95f52c4a
Mohammed Jaffer <mohammed.strive@outlook.com>
2022-12-15 10:35:40 +0530

    refactor(Mohammed Jaffer): add type definition

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,23 +3,17 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import openBle from 'react-native-openid4vp-ble';
 
-const { Wallet, Verifier, Openid4vpBle } = openBle;
+const { Openid4vpBle } = openBle;
 
 export default function App() {
-  const [walletName, setWalletName] = React.useState<string>();
-  const [verifierName, setVerifierName] = React.useState<string>();
   const [connectionParams, setConnectionParams] = React.useState<string>();
 
   React.useEffect(() => {
-    Wallet.getModuleName().then((name: string) => setWalletName(name));
-    Verifier.getModuleName().then((name: string) => setVerifierName(name));
     setConnectionParams(Openid4vpBle.getConnectionParameters());
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Wallet Name: {walletName}</Text>
-      <Text>Verifier Name: {verifierName}</Text>
       <Text>Connection Params: {connectionParams}</Text>
     </View>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { NativeModules, Platform } from 'react-native';
+import type { OpenIDBLEShare } from './types/bleshare';
 
 const LINKING_ERROR =
   `The package 'react-native-openid4vp-ble' doesn't seem to be linked. Make sure: \n\n` +
@@ -6,7 +7,7 @@ const LINKING_ERROR =
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo Go\n';
 
-const Openid4vpBle = NativeModules.Openid4vpBle
+const Openid4vpBle: OpenIDBLEShare = NativeModules.Openid4vpBle
   ? NativeModules.Openid4vpBle
   : new Proxy(
       {},
