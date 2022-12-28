651dfed8396556c95d8d5d94374e575e05d314ed
Tilak Puli <tilakpuli15@gmail.com>
2022-12-28 19:59:50 +0530

    chore(#305): add transmission report class

---
+++ android/src/main/java/io/mosip/tuvali/transfer/TransmissionReport.kt
@@ -0,0 +1,37 @@
+package io.mosip.tuvali.transfer
+
+class TransmissionReport  {
+  private val type: ReportType
+  private val  totalPages: Int
+  private val missingSequences: IntArray?
+
+  public enum class ReportType {
+    MISSING_CHUNKS,
+    SUCCESS
+  }
+
+  constructor(type: ReportType, totalPages: Int, missingSequences: IntArray?) {
+    this.type = type
+    this.totalPages = totalPages
+    this.missingSequences = missingSequences
+  }
+
+  constructor(bytes: ByteArray){
+    this.type = ReportType.values()[bytes[0].toInt()]
+    this.totalPages = Util.twoBytesToIntBigEndian(byteArrayOf(bytes[1], bytes[2]))
+    this.missingSequences = bytes.drop(3).chunked(2)
+                            .fold(intArrayOf())
+                            { acc, twoBytes -> acc + Util.twoBytesToIntBigEndian(twoBytes.toByteArray())}
+  }
+
+  fun toByteArray(): ByteArray {
+    val missingSeqBytes = missingSequences?.fold(byteArrayOf()) { acc, sNo -> acc + Util.intToTwoBytesBigEndian(sNo) }
+    val metadata = byteArrayOf(type.ordinal.toByte(), totalPages.toByte())
+
+    return if(missingSeqBytes != null) {
+      metadata + missingSeqBytes
+    } else {
+      metadata
+    }
+  }
+}
