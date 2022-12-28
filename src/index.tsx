c3fa63a529c17f42b5ea592ccbcc9592dc860814
Mohammed Jaffer <mohammed.strive@outlook.com>
2022-12-28 15:39:20 +0530

    Revert "feat(#305): adding compression at javascript layer for send function"
    This reverts commit 943812ad979fda641582c7083f7c673f8db21e3d.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
 import type { OpenIDBLEShare } from './types/bleshare';
-import { strFromU8, strToU8, compressSync } from 'fflate';
 
 const LINKING_ERROR =
   `The package 'react-native-openid4vp-ble' doesn't seem to be linked. Make sure: \n\n` +
@@ -19,23 +18,14 @@ const Openid4vpBle: OpenIDBLEShare = NativeModules.Openid4vpBle
       }
     );
 
-const newOpenid4vpBle: OpenIDBLEShare = {
-  ...Openid4vpBle,
-  send(message: string, callback: () => void) {
-    const messageBytes = strToU8(message, false);
-    const compressedMessage = compressSync(messageBytes, { level: 6, mem: 8 });
-    Openid4vpBle.send(strFromU8(compressedMessage, false), callback);
-  },
-};
-
 if (Platform.OS === 'android') {
   const eventEmitter = new NativeEventEmitter();
-  newOpenid4vpBle.handleNearbyEvents = (callback) =>
+  Openid4vpBle.handleNearbyEvents = (callback) =>
     eventEmitter.addListener('EVENT_NEARBY', callback);
-  newOpenid4vpBle.handleLogEvents = (callback) =>
+  Openid4vpBle.handleLogEvents = (callback) =>
     eventEmitter.addListener('EVENT_LOG', callback);
 }
 
 export default {
-  newOpenid4vpBle,
+  Openid4vpBle,
 };
