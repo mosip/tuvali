664ad4a53d8a3ea03823722778c6ad2baf35f6a4
Mohammed Jaffer <mohammed.strive@outlook.com>
2023-01-05 16:35:53 +0530

    feat(#305): adding iOS ble central and peripheral delegates

--- ios/ble/central/Central.swift
+++ ios/ble/central/Central.swift
@@ -12,9 +12,23 @@ import CoreBluetooth
 class Central: NSObject {
     private var centralManager: CBCentralManager!
     
+    var connectedPeripheral: CBPeripheral?
+    
     override init() {
         super.init()
         centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    func scanForPeripherals() {
+        centralManager.scanForPeripherals(withServices: [Peripheral.SERVICE_UUID, Peripheral.SCAN_RESPONSE_SERVICE_UUID], options: [CBCentralManagerScanOptionAllowDuplicatesKey: false])
+    }
+    
+    func connectToPeripheral(peripheral: CBPeripheral) {
+        os_log("Coonecting to peripheral")
+        centralManager.connect(peripheral)
+    }
+    
+    func writeData(message: String) {
     }
 }
 
--- ios/ble/peripheral/Peripheral.swift
+++ ios/ble/peripheral/Peripheral.swift
@@ -1,22 +1,21 @@
-//
-//  Peripheral.swift
-//  Openid4vpBle
-//
-//  Created by Shaik mohammed Jaffer on 03/01/23.
-//  Copyright © 2023 Facebook. All rights reserved.
-//
-
 import Foundation
 import CoreBluetooth
 
 class Peripheral: NSObject {
     private var peripheralManager: CBPeripheralManager!
     
+    static let SERVICE_UUID = CBUUID(string: "0000AB29-0000-1000-8000-00805f9b34fb")
+    static let SCAN_RESPONSE_SERVICE_UUID = CBUUID(string: "0000AB2A-0000-1000-8000-00805f9b34fb")
+    
     override init() {
         super.init()
         peripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: [CBPeripheralManagerOptionShowPowerAlertKey: true])
     }
     
-    internal func setupPeripheral() {
+    func setupPeripheral() {
+        let bleService = CBMutableService(type: Self.SERVICE_UUID, primary: Self.SCAN_RESPONSE_SERVICE_UUID)
+        bleService.characteristics = Utils.createCBMutableCharacteristics()
+        
+        peripheralManager.add(bleService)
     }
 }
