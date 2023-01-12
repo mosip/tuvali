d3965f2eef8b7b5f9c8a92b12ea1cd84f5dd63d1
Harsh Vardhan <harsh59v@gmail.com>
2023-01-12 11:42:29 +0530

    feat(#305): add verification before connecting

--- ios/Wallet/Wallet.swift
+++ ios/Wallet/Wallet.swift
@@ -6,8 +6,14 @@ class Wallet: NSObject {
     static let shared = Wallet()
     var advIdentifier: String?
     var central: Central = Central()
-
+    var secretTranslator: SecretTranslator?
+    
     private override init() {}
+    
+    func setSecretTranslator(ss: SecretTranslator){
+        print("ss called")
+        secretTranslator = ss
+    }
     
     @objc(getModuleName:withRejecter:)
     func getModuleName(resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void {
@@ -33,5 +39,30 @@ class Wallet: NSObject {
     
     func startScanning() {
         central.scanForPeripherals()
+    }
+    
+    func isSameAdvIdentifier(advertisementPayload: Data) -> Bool {
+        // let advertisementPayloadStr = String(decoding: advertisementPayload, as: UTF8.self)
+        let advIdentiferData = hexStringToData(string: advIdentifier!)
+        print("::: Ad payload::::", advertisementPayload,", Add Identifier ::::", advIdentiferData)
+        if advIdentiferData == advertisementPayload {
+            return true
+        }
+        return false
+    }
+
+    func hexStringToData(string: String) -> Data {
+        let stringArray = Array(string)
+        var data: Data = Data()
+        for i in stride(from: 0, to: string.count, by: 2) {
+            let pair: String = String(stringArray[i]) + String(stringArray[i+1])
+                if let byteNum = UInt8(pair, radix: 16) {
+                    let byte = Data([byteNum])
+                    data.append(byte)
+                } else {
+                    fatalError()
+                }
+        }
+        return data
     }
 }
