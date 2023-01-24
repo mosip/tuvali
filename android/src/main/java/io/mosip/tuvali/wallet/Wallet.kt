e191fa56170a999b27e5a84e0095c9f0aad705b1
Tilak Puli <tilakpuli15@gmail.com>
2023-01-24 11:17:03 +0530

    feat(#305): stop scan only after MTU update

--- android/src/main/java/io/mosip/tuvali/wallet/Wallet.kt
+++ android/src/main/java/io/mosip/tuvali/wallet/Wallet.kt
@@ -103,8 +103,6 @@ class Wallet(
 
     //TODO: Handle multiple calls while connecting
     if (advertisementPayload != null && isSameAdvIdentifier(advertisementPayload) && scanResponsePayload != null) {
-      Log.d(logTag, "Stopping the scan.")
-      central.stopScan()
 
       setVerifierPK(advertisementPayload, scanResponsePayload)
       central.connect(device)
@@ -148,6 +146,9 @@ class Wallet(
 
   override fun onRequestMTUSuccess(mtu: Int) {
     Log.d(logTag, "onRequestMTUSuccess")
+    Log.d(logTag, "Stopping the scan.")
+    central.stopScan()
+
     //TODO: Can we pass this MTU value to chunker, would this callback always come?
     val connectionEstablishedCallBack = callbacks[CentralCallbacks.CONNECTION_ESTABLISHED]
     central.subscribe(Verifier.SERVICE_UUID, GattService.CONNECTION_STATUS_CHANGE_CHAR_UUID)
