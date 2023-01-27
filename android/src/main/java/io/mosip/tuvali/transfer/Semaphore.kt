1b2f16675441fcab4e2b2d52cdfa6b8ef2f068e6
Tilak Puli <34330361+tilak-puli@users.noreply.github.com>
2023-01-27 10:42:49 +0530

    Revert "feat(#444): [Bhargavi|Adityan] [Android] refactor tuvali gatt service to match the spec"

--- android/src/main/java/io/mosip/tuvali/transfer/Semaphore.kt
+++ android/src/main/java/io/mosip/tuvali/transfer/Semaphore.kt
@@ -1,7 +1,7 @@
 package io.mosip.tuvali.transfer
 
-class TransferReportRequest {
-  enum class ReportType {
+class Semaphore {
+  enum class SemaphoreMarker {
     UnInitialised,
     RequestReport,
     Error
--- android/src/main/java/io/mosip/tuvali/verifier/GattService.kt
+++ android/src/main/java/io/mosip/tuvali/verifier/GattService.kt
@@ -7,15 +7,14 @@ import java.util.*
 class GattService {
   //TODO: Update UUIDs as per specification
   companion object {
-    val IDENTIFY_REQUEST_CHAR_UUID: UUID = UUID.fromString("00002030-0000-1000-8000-00805f9b34fb")
+    val IDENTITY_CHARACTERISTIC_UUID: UUID = UUID.fromString("00002030-0000-1000-8000-00805f9b34fb")
     val REQUEST_SIZE_CHAR_UUID: UUID = UUID.fromString("00002031-0000-1000-8000-00805f9b34fb")
     val REQUEST_CHAR_UUID: UUID = UUID.fromString("00002032-0000-1000-8000-00805f9b34fb")
     val RESPONSE_SIZE_CHAR_UUID: UUID = UUID.fromString("00002033-0000-1000-8000-00805f9b34fb")
-    val SUBMIT_RESPONSE_CHAR_UUID: UUID = UUID.fromString("00002034-0000-1000-8000-00805f9b34fb")
-    val TRANSFER_REPORT_REQUEST_CHAR_UUID: UUID = UUID.fromString("00002035-0000-1000-8000-00805f9b34fb")
-    val TRANSFER_REPORT_RESPONSE_CHAR_UUID: UUID = UUID.fromString("00002036-0000-1000-8000-00805f9b34fb")
-    val VERIFICATION_STATUS_CHAR_UUID: UUID = UUID.fromString("00002037-0000-1000-8000-00805f9b34fb")
-    val DISCONNECT_CHAR_UUID: UUID = UUID.fromString("00002038-0000-1000-8000-00805f9b34fb")
+    val RESPONSE_CHAR_UUID: UUID = UUID.fromString("00002034-0000-1000-8000-00805f9b34fb")
+    val SEMAPHORE_CHAR_UUID: UUID = UUID.fromString("00002035-0000-1000-8000-00805f9b34fb")
+    val VERIFICATION_STATUS_CHAR_UUID: UUID = UUID.fromString("00002036-0000-1000-8000-00805f9b34fb")
+    val CONNECTION_STATUS_CHANGE_CHAR_UUID: UUID = UUID.fromString("00002037-0000-1000-8000-00805f9b34fb")
   }
 
   fun create(): BluetoothGattService {
@@ -24,9 +23,9 @@ class GattService {
       BluetoothGattService.SERVICE_TYPE_PRIMARY
     )
 
-    val identifyRequestChar = BluetoothGattCharacteristic(
-      IDENTIFY_REQUEST_CHAR_UUID,
-      BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE,
+    val identityChar = BluetoothGattCharacteristic(
+      IDENTITY_CHARACTERISTIC_UUID,
+      BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE or BluetoothGattCharacteristic.PROPERTY_WRITE,
       BluetoothGattCharacteristic.PERMISSION_WRITE
     )
 
@@ -44,49 +43,42 @@ class GattService {
 
     val responseSizeChar = BluetoothGattCharacteristic(
       RESPONSE_SIZE_CHAR_UUID,
-      BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE,
+      BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE or BluetoothGattCharacteristic.PROPERTY_WRITE,
       BluetoothGattCharacteristic.PERMISSION_WRITE
     )
 
-    val submitResponseChar = BluetoothGattCharacteristic(
-      SUBMIT_RESPONSE_CHAR_UUID,
-      BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE,
+    val responseChar = BluetoothGattCharacteristic(
+      RESPONSE_CHAR_UUID,
+      BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE or BluetoothGattCharacteristic.PROPERTY_WRITE,
       BluetoothGattCharacteristic.PERMISSION_WRITE
     )
 
-    val transferReportRequestChar = BluetoothGattCharacteristic(
-      TRANSFER_REPORT_REQUEST_CHAR_UUID,
-      BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE,
-      BluetoothGattCharacteristic.PERMISSION_WRITE
-    )
-
-    val transferReportResponseChar = BluetoothGattCharacteristic(
-      TRANSFER_REPORT_RESPONSE_CHAR_UUID,
-       BluetoothGattCharacteristic.PROPERTY_INDICATE,
-      BluetoothGattCharacteristic.PERMISSION_READ
+    val semaphoreChar = BluetoothGattCharacteristic(
+      SEMAPHORE_CHAR_UUID,
+      BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE or BluetoothGattCharacteristic.PROPERTY_WRITE or BluetoothGattCharacteristic.PROPERTY_READ or BluetoothGattCharacteristic.PROPERTY_INDICATE,
+      BluetoothGattCharacteristic.PERMISSION_READ or BluetoothGattCharacteristic.PERMISSION_WRITE
     )
 
     val verificationStatusChar = BluetoothGattCharacteristic(
       VERIFICATION_STATUS_CHAR_UUID,
-      BluetoothGattCharacteristic.PROPERTY_INDICATE,
+      BluetoothGattCharacteristic.PROPERTY_READ or BluetoothGattCharacteristic.PROPERTY_INDICATE,
       BluetoothGattCharacteristic.PERMISSION_READ
     )
 
-    val disconnectStatusChar = BluetoothGattCharacteristic(
-      DISCONNECT_CHAR_UUID,
-      BluetoothGattCharacteristic.PROPERTY_INDICATE,
+    val connectionStatusChar = BluetoothGattCharacteristic(
+      CONNECTION_STATUS_CHANGE_CHAR_UUID,
+      BluetoothGattCharacteristic.PROPERTY_READ or BluetoothGattCharacteristic.PROPERTY_INDICATE,
       BluetoothGattCharacteristic.PERMISSION_READ
     )
 
-    service.addCharacteristic(identifyRequestChar)
+    service.addCharacteristic(identityChar)
     service.addCharacteristic(requestSizeChar)
     service.addCharacteristic(requestChar)
     service.addCharacteristic(responseSizeChar)
-    service.addCharacteristic(submitResponseChar)
-    service.addCharacteristic(transferReportRequestChar)
-    service.addCharacteristic(transferReportResponseChar)
+    service.addCharacteristic(responseChar)
+    service.addCharacteristic(semaphoreChar)
     service.addCharacteristic(verificationStatusChar)
-    service.addCharacteristic(disconnectStatusChar)
+    service.addCharacteristic(connectionStatusChar)
 
     return service
   }
