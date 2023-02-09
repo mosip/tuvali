bc5bb12e993d1f9e2d88fa27a0714a35e9c06639
Ravi Kumar P <292991+ravikp@users.noreply.github.com>
2023-02-09 17:25:26 +0530

    feat(#497): implement CRC-16 Kermit algorithm in accordance to OpenId4VP BLE specification
    The kermit algorithm is implemented in BLE 5.0 specification. OpenId4VP also takes
    the same algorithm in the higher layers for its checksum match

--- android/src/main/java/io/mosip/tuvali/transfer/CheckValue.kt
+++ android/src/main/java/io/mosip/tuvali/transfer/CheckValue.kt
@@ -7,8 +7,13 @@ import com.github.snksoft.crc.CRC
 object CheckValue {
   private const val logTag = "CheckValue"
 
+  //CRC-16/Kermit: https://reveng.sourceforge.io/crc-catalogue/16.htm#crc.cat.crc-16-kermit
+  //width=16 poly=0x1021 init=0x0000 refin=true refout=true xorout=0x0000 check=0x2189 residue=0x0000 name="CRC-16/KERMIT"
+  //TODO: Need to identify what is check, and residue in the Kermit algorithm
+  private val crc16KermitParameters = CRC.Parameters(16, 0x1021, 0x0000, true, true, 0x0000)
+
   fun get(data: ByteArray): UShort {
-    return CRC.calculateCRC(CRC.Parameters.CCITT, data).toUShort()
+    return CRC.calculateCRC(crc16KermitParameters, data).toUShort()
   }
 
   fun verify(data: ByteArray, receivedCRC: UShort) : Boolean {
