b6c35a62c903189958d2a0eef639f2fbac291586
Alka Prasad <prasadalka1998@gmail.com>
2023-02-10 16:42:28 +0530

    fix(#409): hardcode mtu and chunker size to 185 for android to android transfer

--- android/src/main/java/io/mosip/tuvali/wallet/Wallet.kt
+++ android/src/main/java/io/mosip/tuvali/wallet/Wallet.kt
@@ -44,8 +44,7 @@ class Wallet(
   private val handlerThread = HandlerThread("TransferHandlerThread", Process.THREAD_PRIORITY_DEFAULT)
 
   private var central: Central
-  //Reduced 2 from 517 to be on safer side
-  private val maxMTU = 515
+  private val maxMTU = 185
 
   private val retryDiscoverServices = BackOffStrategy(maxRetryLimit = 5)
 
