e30b2f1bfa2873c0c04fea22a3c6a4c2d1b46ece
Tilak Puli <tilakpuli15@gmail.com>
2023-04-20 11:02:36 +0530

    feat(#52): pass exception as it is to exception handler instead of wrapping it with unknown

--- android/src/main/java/io/mosip/tuvali/common/safeExecute/TryExecuteSync.kt
+++ android/src/main/java/io/mosip/tuvali/common/safeExecute/TryExecuteSync.kt
@@ -1,22 +1,21 @@
 package io.mosip.tuvali.common.safeExecute
 
 import io.mosip.tuvali.openid4vpble.exception.OpenIdBLEExceptionHandler
-import io.mosip.tuvali.openid4vpble.exception.UnknownException
 
 class TryExecuteSync(private val bleExceptionHandler: OpenIdBLEExceptionHandler) {
   private val mutex = Object()
 
   fun <T> run(fn: () -> T): T? {
-    var returnValue: T? = null;
+    var returnValue: T? = null
 
     synchronized(mutex) {
       try {
         returnValue = fn()
       } catch (e: Exception) {
-        bleExceptionHandler.handleException(UnknownException("Caught unknown exception in Try Execute", e))
+        bleExceptionHandler.handleException(e)
       }
     }
 
-    return returnValue;
+    return returnValue
   }
 }
