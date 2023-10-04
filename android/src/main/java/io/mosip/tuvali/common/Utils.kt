a593a3f9391aca9bbfc25403738566cefcee41bf
Sreenadh S <32409698+sree96@users.noreply.github.com>
2023-10-04 12:46:29 +0530

    refactor(MOSIP-29218): formatting

--- android/src/main/java/io/mosip/tuvali/common/Utils.kt
+++ android/src/main/java/io/mosip/tuvali/common/Utils.kt
@@ -3,9 +3,9 @@ package io.mosip.tuvali.common
 import java.nio.ByteBuffer
 
 class Utils {
-  companion object{
+  companion object {
     fun longToBytes(value: Long): ByteArray {
-       val buffer: ByteBuffer = ByteBuffer.allocate(Long.SIZE_BYTES)
+      val buffer: ByteBuffer = ByteBuffer.allocate(Long.SIZE_BYTES)
       buffer.putLong(value)
       return buffer.array()
     }
