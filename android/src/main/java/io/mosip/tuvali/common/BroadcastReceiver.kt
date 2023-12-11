297363778a86336f804fc87913fc700af9968e36
Kiruthika Jeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2023-12-11 10:34:32 +0530

    refactor(INJI-156): remove debug log
    Signed-off-by: Kiruthika Jeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>

--- android/src/main/java/io/mosip/tuvali/common/BroadcastReceiver.kt
+++ android/src/main/java/io/mosip/tuvali/common/BroadcastReceiver.kt
@@ -4,7 +4,6 @@ import android.bluetooth.BluetoothAdapter
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import io.mosip.tuvali.common.events.EventEmitter
 import io.mosip.tuvali.transfer.Util
 import io.mosip.tuvali.wallet.IWallet
 
@@ -12,12 +11,9 @@ open class BroadcastReceiver(
   private val wallet: IWallet,
 ) : android.content.BroadcastReceiver() {
   private val logTag = Util.getLogTag(javaClass.simpleName)
-  private var eventEmitter = EventEmitter()
-  override fun onReceive(p0: Context?, intent: Intent?) {
-    Log.d(logTag, "BroadcastReceiver overridden onReceive $p0 ${intent?.extras}")
+  override fun onReceive(context: Context?, intent: Intent?) {
     val action = intent?.action
     if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
-
       val state: Int? = intent?.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR)
       when (state) {
         BluetoothAdapter.STATE_OFF -> {
